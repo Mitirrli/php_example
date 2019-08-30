94ad42fc395c1906b1bcf6115a26e1d5b5baa8f1
黄磊 <huangl@golive.cc>
2019-08-30 14:13:23 +0800

    配置

--- Framework/config/database.php
+++ Framework/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'DB_HOST' => env(DB_PORT),
-    'DB_DATABASE' => env(DB_DATABASE),
-    'DB_USER' => env(DB_USER),
-    'DB_PASSWORD' => env(DB_PASSWORD),
-    'DB_PORT' => env(DB_PORT),
+    'DB_HOST' => env('DB_HOST_'),
+    'DB_DATABASE' => env('DB_DATABASE'),
+    'DB_USER' => env('DB_USER'),
+    'DB_PASSWORD' => env('DB_PASSWORD'),
+    'DB_PORT' => env('DB_PORT'),
 ];
