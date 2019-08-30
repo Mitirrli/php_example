eefb1079e6e0f0a52337e53f71505f0effe98d4f
黄磊 <huangl@golive.cc>
2019-08-30 15:56:27 +0800

    study

--- Framework/application/controllers/TestController.php
+++ Framework/application/controllers/TestController.php
@@ -17,4 +17,22 @@ class TestController extends Controller
         $model = new TestModel();
         $model->test();
     }
+
+    /**
+     * 按位与
+     */
+    public function test1()
+    {
+        $a = 6;//110
+        $b = 3;//011
+//        dd($a & $b);  //2
+        //设置消息提醒的功能  邮箱提醒、短信提醒、公众号提醒
+
+
+//        dd($a & 1);
+//        将整数与1 按位与 当第一位为1的都是奇数
+
+        //1、2、4
+        dd(5 & 2);
+    }
 }
--- Framework/config/route.php
+++ Framework/config/route.php
@@ -2,4 +2,7 @@
 
 return [
     'test' => ['app\controllers\TestController@test', 'post'],
+    'test1' => ['app\controllers\TestController@test1', 'post'],
+    'test2' => ['app\controllers\TestController@test2', 'post'],
+
 ];
